37fc4dede644cdf5129c8b50fe4607bee354a3bb
MW_GR <mwpark@rabahgroow.com>
2022-12-09 17:23:22 +0900

    [FIX] mobile size

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -6,7 +6,7 @@ import { TodoProvider } from '../context/TodoContext';
 export default function Container() {
   return (
     <TodoProvider>
-      <div className='absolute top-[64px] left-[1.7rem] md:top-[25%] md:left-[40%]'>
+      <div className='absolute top-[80px] left-[1.7rem] md:top-[25%] md:left-[40%]'>
         <Header className='fixed' />
         <Todo className='fixed' />
         <Footer className='fixed' />

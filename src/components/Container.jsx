3ba62093d957ce557cb28eec1a72e2335ab6a060
MW_GR <mwpark@rabahgroow.com>
2022-12-09 17:03:02 +0900

    [FIX] mobile keyboard issue

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -6,7 +6,7 @@ import { TodoProvider } from '../context/TodoContext';
 export default function Container() {
   return (
     <TodoProvider>
-      <div className='absolute top-[64px] left-[1.7rem] md:top-[25%] md:left-[40%]'>
+      <div className='fixed top-[64px] left-[1.7rem] md:top-[25%] md:left-[40%]'>
         <Header />
         <Todo />
         <Footer />
